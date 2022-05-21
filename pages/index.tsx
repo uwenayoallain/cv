4b84817e334951e1c15c8b38b7c95c2277dd1824
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2022-05-21 11:34:09 +0200

    setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from "next";
+import { Container } from "components/components";
 
 const Home: NextPage = () => {
-  return <div>test</div>;
+  return (
+    <Container>
+      <h1>Hello Next.js</h1>
+    </Container>
+  );
 };
 
 export default Home;
