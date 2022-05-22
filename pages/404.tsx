445469cf84519ba7133e2b153b1a7538157231f2
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2022-05-22 21:47:42 +0200

    continue

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import { Container } from "components/components";
+
+export default function CustomErrorPage() {
+  return (
+    <Container>
+      <h1>404</h1>
+      <p>Page not found</p>
+    </Container>
+  );
+}
