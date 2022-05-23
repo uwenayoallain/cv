3884809baf9c8ad96b4e14b63553dc980e7fdc26
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2022-05-23 21:17:21 +0200

    using sass insteadof styled-components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,10 @@
 import type { NextPage } from "next";
-import { Container } from "components/components";
 
 const Home: NextPage = () => {
   return (
-    <Container>
-      <div>
-        <h1>Home</h1>
-        <p>This is the home page</p>
-      </div>
-    </Container>
+    <div>
+      <p>Hello Next.js</p>
+    </div>
   );
 };
 
