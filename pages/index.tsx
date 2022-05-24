a14af49d9f4d275a84ff77bfde3959567c9d7a88
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2022-05-24 20:42:42 +0200

    done

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,17 @@ const Home: NextPage = () => {
   return (
     <div>
       <section className='header_section'>
-        <div className='logo' />
-        <div>
-          <Link href='/'  />
+        <div className='flex justify-content'>
+          <div className='logo' />
+          <div>
+            <Link href='/'>How It Works</Link>
+            <Link href='/'>FAQ</Link>
+            <Link href='/'>About</Link>
+            <Link href='/'>Contact</Link>
+          </div>
+          <button className='btn'>
+            <Link href='/'>Login</Link>
+          </button>
         </div>
       </section>
     </div>
