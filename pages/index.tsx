c4f6ea1954a3fecd2a3a946d1cb644a1fcbe7d3e
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2022-05-24 20:32:43 +0200

    working on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
     <div>
-      <p>Hello Next.js</p>
-      <div className='logo'></div>
+      <section className='header_section'>
+        <div className='logo' />
+        <div>
+          <Link href='/'  />
+        </div>
+      </section>
     </div>
   );
 };
