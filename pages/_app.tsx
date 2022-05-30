c7586ac31902ba008d1b60dd2acdb7e1a0c1683f
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2022-05-30 19:13:55 +0200

    cont

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,8 @@ import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  // add a custom title for all components
+  return <Component {...pageProps} title='Yarison | allain' />;
 }
 
 export default MyApp;
